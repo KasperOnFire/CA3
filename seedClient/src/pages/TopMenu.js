6ca553456a745b014569059f7979c725d96515e4
SpankyHanky <kornholt@hotmail.com>
2017-11-01 18:13:41 +0100

    Added fix to topmenu

--- seedClient/src/pages/TopMenu.js
+++ seedClient/src/pages/TopMenu.js
@@ -31,8 +31,8 @@ class TopMenu extends Component {
             <ul className="nav navbar-nav">
 			  <li><Link to="/about">About</Link></li>
 			  <li><Link to="/register">Register</Link></li>
-              <li><Link to="/user">Page for Users </Link></li>
-              <li><Link to="/admin">Page for Admins</Link></li>
+              {this.state.isUser && (<li><Link to="/user">Page for Users </Link></li>)}
+              {this.state.isAdmin && (<li><Link to="/admin">Page for Admins </Link></li>)}
             </ul>
             <ul className="nav navbar-nav navbar-right">
               <li className="navbar-text" style={{ color: "steelBlue" }}>{logInStatus}</li>
