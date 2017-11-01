b2d05877405273d1fa265d8adc269cbebe3c2c1d
kasperonfire <breindal70@gmail.com>
2017-11-01 18:57:26 +0100

    tried fixing admin seeing all users

--- seedClient/src/pages/TopMenu.js
+++ seedClient/src/pages/TopMenu.js
@@ -33,6 +33,7 @@ class TopMenu extends Component {
 			  <li><Link to="/register">Register</Link></li>
               {this.state.isUser && (<li><Link to="/user">Page for Users </Link></li>)}
               {this.state.isAdmin && (<li><Link to="/admin">Page for Admins </Link></li>)}
+              {this.state.isAdmin && (<li><Link to="/users">All Users</Link></li>)}
             </ul>
             <ul className="nav navbar-nav navbar-right">
               <li className="navbar-text" style={{ color: "steelBlue" }}>{logInStatus}</li>
---
+++ seedClient/src/pages/Users.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import auth from "../authorization/auth";
+
+class RegisterUser extends Component{
+
+	constructor() {
+		super();
+		this.state = { err: "", user: {username:"", password:""}};
+	  }
+
+	  componentDidMount(){
+		var apiLink = "http://localhost:8084/seedMaven/api/demoadmin/getAll";
+		var promise = fetch(apiLink);
+		var dataField = document.getElementById("data");
+		promise.then(function (response) {
+			var userData = response.json();
+			return userData;
+		}).then(function (data) {
+			var htmlData = data.map(function (d) {
+				return "<p>username: " + d.username + "</p>\n";
+			});
+			dataField.innerHTML = htmlData;
+			console.log(htmlData);
+	  });
+	}
+
+	 
+	render() {
+		return (
+		<div className="container">
+		<h2>All users</h2>
+			<div id="data">
+			</div>
+      </div>
+	);
+  }
+}
+
+export default RegisterUser	;
