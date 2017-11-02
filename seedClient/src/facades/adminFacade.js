873e0e9813f8376e8f4d52ffecb7565b3761fb09
kasperonfire <breindal70@gmail.com>
2017-11-02 11:10:58 +0100

    removed users

--- seedClient/src/facades/adminFacade.js
+++ seedClient/src/facades/adminFacade.js
@@ -1,54 +1,37 @@
-import fetchHelper, {errorChecker} from "./fetchHelpers"
+import fetchHelper, {
+	errorChecker
+} from "./fetchHelpers"
 const URL = require("../../package.json").serverURL;
 
 
 class AdminStore {
-  constructor() {
-    this._data = "";
-    this._errorMessage = "";
-  }
+	constructor() {
+		this._data = "";
+		this._errorMessage = "";
+	}
 
-  getData = (cb) => {
-    this._errorMessage = "";
-    this._messageFromServer = "";
-    let resFromFirstPromise=null;  //Pass on response the "second" promise so we can read errors from server
-    const options = fetchHelper.makeOptions("GET", true);
-    fetch(URL + "api/demoadmin", options)
-      .then((res) => {
-        resFromFirstPromise = res;
-        return res.json();
-      }).then((data) => {
-        errorChecker(resFromFirstPromise,data);
-        if (cb) {
-          cb(null, data)
-        }
-      }).catch(err => {
-        if (cb) {
-          cb({ err: fetchHelper.addJustErrorMessage(err) })
-        }
-      })
-  }
-
-  getAllUsers = (cb) => {
-	this._errorMessage = "";
-    this._messageFromServer = "";
-    let resFromFirstPromise=null;  //Pass on response the "second" promise so we can read errors from server
-    const options = fetchHelper.makeOptions("GET", true);
-    fetch(URL + "api/demoadmin/getAll", options)
-      .then((res) => {
-        resFromFirstPromise = res;
-        return res.json();
-      }).then((data) => {
-        errorChecker(resFromFirstPromise,data);
-        if (cb) {
-          cb(null, data.message)
-        }
-      }).catch(err => {
-        if (cb) {
-          cb({ err: fetchHelper.addJustErrorMessage(err) })
-        }
-      })
-  }
+	getData = (cb) => {
+		this._errorMessage = "";
+		this._messageFromServer = "";
+		let resFromFirstPromise = null; //Pass on response the "second" promise so we can read errors from server
+		const options = fetchHelper.makeOptions("GET", true);
+		fetch(URL + "api/demoadmin", options)
+			.then((res) => {
+				resFromFirstPromise = res;
+				return res.json();
+			}).then((data) => {
+				errorChecker(resFromFirstPromise, data);
+				if (cb) {
+					cb(null, data)
+				}
+			}).catch(err => {
+				if (cb) {
+					cb({
+						err: fetchHelper.addJustErrorMessage(err)
+					})
+				}
+			})
+	}
 
 }
 
