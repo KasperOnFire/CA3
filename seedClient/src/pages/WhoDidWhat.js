c5c451392404fe685cf27d2f98f6dee681def857
SpankyHanky <kornholt@hotmail.com>
2017-11-05 15:09:50 +0100

    added more documentation

--- seedClient/src/pages/WhoDidWhat.js
+++ seedClient/src/pages/WhoDidWhat.js
@@ -1,8 +1,19 @@
 import React, {Component} from "react";
 
 export default class WhoDidWhat extends Component{
-  render() {
-    return <h1>This is the place where you can see who did what on CA3</h1>
+  render(){ return (<div>
+      <h1>This is the place where you can see who did what on CA3</h1>
+      <p> Due to group issues we ended up doing the excersice with only 2 of our initial 4 members </p>
+      <p> Kasper Vink Jonassen and Kristian Krog left us early on in the week.</p>
+      <p> That is why you only see contribution from Kasper Ravn Breindal and Anton Kornholt</p>
+      
+      <p>The easiest way to view indviduel contribution is through our git commit log.</p>
+      <a href="https://github.com/KasperOnFire/CA3/commits/master">Link to commit log</a>
+      
+      <h3>However here is a brief rundown of what each member did:</h3>
+      </div>);
+    
+    
   }
 }
 
